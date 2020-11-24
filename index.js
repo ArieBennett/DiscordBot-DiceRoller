302045c3fb4a679d4996a5ebd6512d0feb3aaad8
ArieBennett <mr.bennett1995@hotmail.com>
2020-11-24 19:02:48 +1030

    Update index.js
    Adding functions to simulate dice rolls and coins flips.

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ const client = new Discord.Client();
 const prefix = "/r";
 var   botPassword = "your-token-goes-here";   // see if a commandline argument on launch can set this
 
+// roll [num] [sides]-sided dice and add the constant [add]; returns total sum of dice AND constant
+function rollDice(num, sides, add) {
+    var roll = 0;
+
+    for(var i = 0; i < num; i++) {
+        roll += Math.floor(1 + Math.random() * sides);
+    }
+    
+    return (roll + add);
+}
+
+// flip [num] coins; eturns the number of heads.
+function flipCoins(num) {
+    var count = 0;
+
+    for(var i = 0; i < num; i++) {
+        count += Math.floor(Math.random() * 2);
+    }
+
+    return count;
+}
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
